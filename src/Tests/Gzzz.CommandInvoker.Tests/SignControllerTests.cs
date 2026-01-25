9eb8660b190ac52ecbbdb34268bed35882a54743
newp <mynewp@gmail.com>
2026-01-25 19:34:56 +0900

    만료된 refreshtoken을 사용할 때 expired 테스트 추가

--- src/Tests/Gzzz.CommandInvoker.Tests/SignControllerTests.cs
+++ src/Tests/Gzzz.CommandInvoker.Tests/SignControllerTests.cs
@@ -43,18 +43,17 @@ public class SignControllerTests : AwsFunctionUrlHandlerFixture
 		Assert.That(newTokens.RefreshToken, Is.EqualTo(_client.AuthenticationTokens.RefreshToken));
 	}
 	[Test]
-	public async Task RefreshSignTestExpiredAsync()
+	public async Task ExpiredRefreshSignErrorTestAsync()
 	{
 		await GuestSignInTestAsync();
 		var beforeAccessToken = _client.AuthenticationTokens.AccessToken;
 		var beforeRefreshToken = _client.AuthenticationTokens.RefreshToken;
 		
-		_timeService.SetNow(DateTime.UtcNow);
-		var newTokens = await _client.RefreshTokensAsync();
-		Assert.That(newTokens.AccessToken, Is.Not.EqualTo(beforeAccessToken));
-		Assert.That(newTokens.RefreshToken, Is.Not.EqualTo(beforeRefreshToken));
-
-		Assert.That(newTokens.AccessToken, Is.EqualTo(_client.GetAccessToken()));
-		Assert.That(newTokens.RefreshToken, Is.EqualTo(_client.AuthenticationTokens.RefreshToken));
+		_timeService.SetNow(DateTime.MaxValue); //좀 더 정밀한 시간을 넣어주는게 좋다
+		
+		var exception= Assert.ThrowsAsync<HttpException>(_client.RefreshTokensAsync);
+		Assert.That(exception.StatusCode, Is.EqualTo(400)); //401은 인증성공시
+		Assert.That(exception.ErrorCode, Is.EqualTo(0));
+		Assert.That(exception.Message, Is.EqualTo("expired"));
 	}
 }
