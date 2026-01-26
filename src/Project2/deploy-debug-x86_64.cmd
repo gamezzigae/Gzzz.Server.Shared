c86079c20087570309c7cd3a77eb9d697013f70f
newpzz <mynewp@gmail.com>
2026-01-26 19:13:33 +0900

    #dotnet10
    commit a86e116702e8457c9515e98b979473a20e9027e2
    Merge: 47f083a 19b572e
    Author: newpzz <mynewp@gmail.com>
    Date:   Mon Jan 26 19:12:43 2026 +0900
    
        Merge remote-tracking branch 'origin/develop' into feature/dotnet10
    
    commit 47f083a99afac773638f27ae2f4d8723e094f678
    Author: newpzz <mynewp@gmail.com>
    Date:   Mon Jan 26 15:19:32 2026 +0900
    
        dotnet10 build image 지정
    
    commit f8495ed05e555c37451dd7d98564f7d97fe954ee
    Author: newpzz <mynewp@gmail.com>
    Date:   Sat Jan 24 16:53:01 2026 +0900
    
        dotnet10 Project2 파일추가

--- src/Project2/deploy-debug-x86_64.cmd
+++ src/Project2/deploy-debug-x86_64.cmd
@@ -1,23 +1,20 @@
 @echo off
 
-pushd Project1
 
 dotnet lambda deploy-function ^
 --profile "newp" ^
 --region "ap-northeast-1" ^
---configuration "Release" ^
+--configuration "Debug" ^
 --function-name "test3" ^
 --function-runtime "dotnet10" ^
 --framework "net10.0" ^
 --function-memory-size 512 ^
 --function-timeout "5" ^
---function-handler "Project1" ^
+--function-handler "Project2" ^
 --function-architecture "x86_64" ^
 --package-type "Zip" ^
---msbuild-parameters "--self-contained true" ^
---container-image-for-build public.ecr.aws/sam/build-dotnet10:latest
+--msbuild-parameters "--self-contained true"
 
-popd
 
 
 pause
