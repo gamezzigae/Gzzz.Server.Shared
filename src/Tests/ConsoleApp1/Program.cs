23b6154264f026568b32618e8876b6cc810d2a44
newpzz <mynewp@gmail.com>
2026-02-18 19:32:46 +0900

    CommandInfo 벤치마크 base추가

--- src/Tests/ConsoleApp1/Program.cs
+++ src/Tests/ConsoleApp1/Program.cs
@@ -1,87 +1,56 @@
+using Amazon.DynamoDBv2.Model;
+using Gzzz.CommandInvoker;
+using Gzzz.Controllers;
+using Gzzz.Db.DynamoDb;
+using System.Reflection;
 
-
-BenchmarkRunner.Run<JsonBenchmark2>();
+BenchmarkRunner.Run<UseObjectPoolBenchmark>();
 
 [MemoryDiagnoser]
-public class JsonBenchmark2
+public class UseObjectPoolBenchmark : CommandInvokerBenchmarkBase
 {
-	readonly TokenClaims _item = new TokenClaims(9, DateTimeOffset.UtcNow, RandomX.GetRandomText());
+	readonly object _parameter = new object();
 	[Benchmark]
-	public void WithObject()
+	public async Task WithObjectPool()
 	{
-		Json.Serialize(_item);
+		for(int i = 0; i < 100000; i++)
+		{
+			await _commandInfo.InvokeAsync(_serivces, _parameter);
+		}
 	}
-	[Benchmark]
-	public void WithGeneric()
+	[Benchmark] public async Task NoObjectPool()
 	{
-		JsonSerializer.Serialize<TokenClaims>(_item, DefaultConfig.JsonSerializerOptions);
+		for (int i = 0; i < 100000; i++)
+		{
+			await _commandInfo.InvokeAsync2(_serivces, _parameter);
+		}
 	}
 
 }
 
+public class CommandInvokerBenchmarkBase
+{
+	protected readonly TestController _controller = new TestController ();
+	protected readonly CommandInfo _commandInfo;
+	protected readonly IServiceProvider _serivces;
+	public CommandInvokerBenchmarkBase()
+	{
+		Assembly[] assem = [typeof(TestController).Assembly];
+
+		_serivces = new ServiceCollection()
+				.AddCommandInvokers(assem)
+				.BuildWithValidation();
+
+		var commands = _serivces.GetRequiredService<IReadOnlyDictionary<string, CommandInfo>>();
+		_commandInfo= commands["/test"];
+	}
+
+	[Controller(serviceLifetime: ServiceLifetime.Singleton)]
+	public class TestController
+	{
+		[Command("/test")]
+		public Task TestAsync(object _) => Task.CompletedTask;
+	}
 
 
-/*
-EnvironmentX.SetObject(DynamoDbConfig.EnvironmentVariableName, new DynamoDbConfig() { TableName = "TestTable", ReturnConsumedCapacity="TOTAL" });
-
-var services = new ServiceCollection()
-		.AddSingleton<IContextSerializer, JsonContextSerializer>()
-		.AddDynamoDbService()
-		.AddSingleton<DynamoDbService>()
-		.AddSingleton<ITextLogger, JsonLogger>()
-		.AddAwsFallbackCredentials()
-		.BuildWithValidation();
-
-var ddb = services.GetRequiredService<DynamoDbService>();
-
-var pk = "KEY-KJFA";
-var sk1 = "Data-MemoryStream";
-var sk2 = "Data-Base64";
-var map = new Dictionary<string, AttributeValue>();
-//var now = new DateTimeOffset(2026, 2, 13, 0, 0, 0, TimeSpan.FromHours(9));
-
-
-var length = int.Parse(args[0]);
-var random = new Random(1000);
-var bytes = new byte[length];
-random.NextBytes(bytes);
-
-Console.Write(sk1);
-var item1 = await ddb.GetAsync(pk, sk1);
-item1["Value"] = new AttributeValue() { B = new MemoryStream(bytes) };
-await ddb.UpdateAsync(item1, DateTimeOffset.Now);
-
-
-Console.Write(sk2);
-var item2 = await ddb.GetAsync(pk, sk2);
-item2["Value"] = new AttributeValue() { S = Convert.ToBase64String(bytes) };
-await ddb.UpdateAsync(item2, DateTimeOffset.Now);
-
-//Console.WriteLine("bytes.Length:"+     bytes.Length);
-
-
-
-
-//await ddb.InsertItemAsync(pk, sk1, map, now);
-
-//var items = await ddb.GetAttirubtesAsync(pk, sk1);
-
-//random.NextBytes(bytes);
-
-////items["Value"].S = null;
-////items["Value"] = new AttributeValue() { B = new MemoryStream(bytes) };
-
-//items["Value"].B = null;
-//items["Value"] = new AttributeValue() { S = Convert.ToBase64String(bytes) };
-//await ddb.UpdateItemAsync(items, DateTimeOffset.Now);
-
-//var temp = new JsonCompressBenchmark();
-//Console.WriteLine(temp.Case1());
-//Console.WriteLine(await temp.Case2());
-
-//
-
-
-
-Console.WriteLine(	);
-*/
+}
